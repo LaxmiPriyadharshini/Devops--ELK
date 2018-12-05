a0b6eddc22b86ad7b4b92879a3218fa33a0fbc43
yourname <user@email.com>
2018-12-05 18:48:31 -0500

    ELK stack

---
+++ service-create.sh
@@ -0,0 +1,5 @@
+docker service create --log-driver=gelf --log-opt gelf-address=udp://localhost:12201 \
+                      gelf-address=udp://localhost:12202 \
+                      --name petclinic -p 8080:8080 \
+                      --replicas=2 \
+                      lsureshk/petclinic:v1
